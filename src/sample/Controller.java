6b3b6670d1d4ba6f15617e2a0c14a67c482576db
SoberDriver <mralexandrfaq@gmail.com>
2021-04-20 16:12:29 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+
+public class Controller {
+    @FXML
+    TextField mainText;
+
+    @FXML
+    Label mainLabel;
+
+    public void buttonAction(ActionEvent actionEvent) {
+         mainLabel.setText(mainLabel.getText() + "\n" + mainText.getText());
+         mainText.setText("");
+    }
+
+    public void fieldAction(ActionEvent actionEvent) {
+        mainLabel.setText(mainLabel.getText() + "\n" + mainText.getText());
+        mainText.setText("");
+    }
+}
